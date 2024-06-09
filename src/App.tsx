f0b227dc8420e37ea03083f28d44af41151c666c
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-06-09 02:41:27 +0200

    Updates: - zustand useStore to handle context; - link (redToo) and info hotspots images; - config.json fixed;

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import MarzipanoPage from "./components/MarzipanoPage";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useStore } from "./context/useStore";
 
 const App = () => {
-  const [currentSceneIndex, setCurrentSceneIndex] = useState<number>(0);
+  const { currentSceneIndex } = useStore();
 
   useEffect(() => {
     console.log("Current index " + currentSceneIndex);
   }, [currentSceneIndex]);
 
-  const switchScene = (index: number) => {
-    setCurrentSceneIndex(index);  // This should correctly update the state
-  };
-
   return (
     <>
-      <MarzipanoPage currentSceneIndex={currentSceneIndex} switchScene={switchScene} />
-      {/* <button 
-        onClick={() => { switchScene((currentSceneIndex + 1) % 5) }} // Rotate through five scenes
-        className="absolute top-10 left-10 z-50 px-4 py-2 bg-blue-500 text-white rounded shadow"
-      >
-        Switch Scene
-      </button> */}
+      <MarzipanoPage />
     </>
   );
-}
+};
 
 export default App;
