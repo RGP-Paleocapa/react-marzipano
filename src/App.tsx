219966bbbc8ca75d1a23d16b1c74dea53cdb0a61
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-06-09 03:18:32 +0200

    Testing infospot video rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+// App.tsx
 import MarzipanoPage from "./components/MarzipanoPage";
 import { useEffect } from "react";
 import { useStore } from "./context/useStore";
+import VideoOverlay from "./components/VideoOverlay";
 
 const App = () => {
-  const { currentSceneIndex } = useStore();
+  const { currentSceneIndex, videoLink, isVideoVisible, closeVideo } = useStore();
 
   useEffect(() => {
     console.log("Current index " + currentSceneIndex);
@@ -12,6 +14,7 @@ const App = () => {
   return (
     <>
       <MarzipanoPage />
+      {isVideoVisible && videoLink && <VideoOverlay videoLink={videoLink} onClose={closeVideo} />}
     </>
   );
 };
---
+++ src/components/VideoOverlay.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface VideoOverlayProps {
+  videoLink: string;
+  onClose: () => void;
+}
+
+const VideoOverlay: React.FC<VideoOverlayProps> = ({ videoLink, onClose }) => {
+  const isYouTubeLink = (url: string) => {
+    return url.includes('youtube.com') || url.includes('youtu.be');
+  };
+
+  const getYouTubeEmbedUrl = (url: string) => {
+    const videoIdMatch = url.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/);
+    return videoIdMatch ? `https://www.youtube.com/embed/${videoIdMatch[1]}` : '';
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+      <div className="relative w-full max-w-3xl p-4 bg-gray-800 rounded-lg">
+        <button 
+          onClick={onClose} 
+          className="absolute -top-2 -right-96 bg-red-600 text-white rounded-full w-14 h-14 flex items-center justify-center"
+        >
+          X
+        </button>
+        {isYouTubeLink(videoLink) ? (
+          <iframe
+            className="iframe-custom-size -translate-x-1/4"
+            src={getYouTubeEmbedUrl(videoLink)}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        ) : (
+          <video controls className="iframe-custom-size">
+            <source src={videoLink} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default VideoOverlay;
