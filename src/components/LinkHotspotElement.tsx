e55a2d9d0736ade9a60d135564c04beafc1ceb24
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-06-10 21:34:40 +0200

    Updated linkspot rotation based on json data

--- src/components/LinkHotspotElement.tsx
+++ src/components/LinkHotspotElement.tsx
@@ -5,14 +5,20 @@ import linkRedImage from '@/assets/icons/linkred.png';
 interface LinkHotspotElementProps {
   switchToScene: () => void;
   isRed: boolean;
+  rotation?: number;
 }
 
-const LinkHotspotElement: React.FC<LinkHotspotElementProps> = ({ switchToScene, isRed }) => {
+const LinkHotspotElement: React.FC<LinkHotspotElementProps> = ({ switchToScene, isRed, rotation }) => {
+  console.log(rotation);
   return (
-    <div className="w-14 p-0 m-0 rounded-3xl flex items-center justify-center" onClick={(e) => {
-      e.stopPropagation();
-      switchToScene();
-    }}>
+    <div
+      className="w-14 p-0 m-0 rounded-3xl flex items-center justify-center"
+      style={{ transform: `rotate(${rotation}deg)` }}
+      onClick={(e) => {
+        e.stopPropagation();
+        switchToScene();
+      }}
+    >
       <img src={isRed ? linkRedImage : linkImage} alt="Link Icon" className="w-14 h-14" />
     </div>
   );
