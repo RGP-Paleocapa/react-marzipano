13985fc18487a06efda349289c27c8b91fde8625
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-06-12 02:22:35 +0200

    Updates: - created auto depandabot pull updater; - added it to .gitignore; - updated videoverlay video; - and added one to config.json

--- src/components/VideoOverlay.tsx
+++ src/components/VideoOverlay.tsx
@@ -6,6 +6,7 @@ interface VideoOverlayProps {
 }
 
 const VideoOverlay: React.FC<VideoOverlayProps> = ({ videoLink, onClose }) => {
+  const baseUrl = "assets/videos";
   const isYouTubeLink = (url: string) => {
     return url.includes('youtube.com') || url.includes('youtu.be');
   };
@@ -17,16 +18,16 @@ const VideoOverlay: React.FC<VideoOverlayProps> = ({ videoLink, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-      <div className="relative w-full max-w-3xl p-4 bg-gray-800 rounded-lg">
+      <div className="relative w-screen lg:w-fit max-w-5xl px-24 lg:p-4 bg-gray-800 rounded-lg">
         <button 
           onClick={onClose} 
-          className="absolute top-8 -right-96 bg-red-600 text-white rounded-full w-14 h-14 flex items-center justify-center animate-pulse"
+          className="absolute top-4 right-4 lg:-top-20 lg:-right-20 bg-red-600 text-white rounded-full w-14 h-14 flex items-center justify-center animate-pulse"
         >
           X
         </button>
         {isYouTubeLink(videoLink) ? (
           <iframe
-            className="iframe-custom-size -translate-x-1/4"
+            className="relative w-[1000px] h-96 max-w-3xl p-4 bg-gray-800 rounded-lg"
             src={getYouTubeEmbedUrl(videoLink)}
             title="YouTube video player"
             frameBorder="0"
@@ -34,8 +35,8 @@ const VideoOverlay: React.FC<VideoOverlayProps> = ({ videoLink, onClose }) => {
             allowFullScreen
           ></iframe>
         ) : (
-          <video controls className="iframe-custom-size">
-            <source src={videoLink} type="video/mp4" />
+          <video controls className="relative w-full max-w-3xl p-4 bg-gray-800 rounded-lg">
+            <source src={`${baseUrl}/${videoLink}`} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         )}
