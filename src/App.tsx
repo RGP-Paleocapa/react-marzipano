711f48f3f297387940bd93be4e7cc3b96f743f1c
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-06-21 05:27:39 +0200

    Removed scenestate context from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 // App.tsx
 import MarzipanoPage from "@/pages/MarzipanoPage";
-import VideoOverlay from "@/components/overlays/VideoOverlay";
-import { useVideoStore } from "@/context/useVideoStore";
 
 const App = () => {
-  const { videoLink, isVideoVisible, closeVideo } = useVideoStore();
 
   return (
     <>
       <MarzipanoPage />
-      {isVideoVisible && videoLink && <VideoOverlay videoLink={videoLink} onClose={closeVideo} />}
     </>
   );
 };
