590ee07bd938b40e663cc13c4588cf041e889028
Faye Kenoua Diop <kenouadiop@gmail.com>
2024-12-14 17:14:14 +0100

    feat(AudioOverlay): Add transparent greenscreen

--- src/components/overlays/AudioOverlay.tsx
+++ src/components/overlays/AudioOverlay.tsx
@@ -10,10 +10,17 @@ const AudioOverlay: React.FC<AudioOverlayProps> = ({ introAudio }) => {
   useEffect(() => {}, [introAudio]);
 
   return introAudio ? (
+  <div>
+    <div className="w-1/5 h-2/5 bg-transparent absolute left-0 bottom-14 z-100">
+    {/* sostituire transparent con green-600 per vedere il greenscreen verde */}
+
+    </div>
     <audio autoPlay controls className="absolute left-0 bottom-0 z-50">
-      <source src={locationAudio} type="audio/mpeg" />
-      Your browser does not support the audio element.
+    <source src={locationAudio} type="audio/mpeg" />
+    Your browser does not support the audio element.
     </audio>
+  </div>
+
   ) : null;
 };
 
