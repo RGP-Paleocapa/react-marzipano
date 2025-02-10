bc5d594828abbea1155705b2c886d2791017fc95
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2025-02-10 09:42:52 +0100

    refactor(Linkspot): refine visibility behavior for mobile and desktop
    - Made the component visible on hover for desktop only
    - Ensured the component is always visible on mobile devices without needing hover
    - Refined the code structure for better readability and maintainability

--- src/components/hotspots/LinkHotspotElement.tsx
+++ src/components/hotspots/LinkHotspotElement.tsx
@@ -12,12 +12,9 @@ const LinkHotspotElement: React.FC<LinkHotspotElementProps> = ({
   setSceneIndex,
   hotspot,
 }) => {
-  const [isHovered, setIsHovered] = React.useState(false);
   return (
     <div
-      className="relative"
-      onMouseOver={() => setIsHovered(true)}
-      onMouseOut={() => setIsHovered(false)}
+      className="relative group"
     >
       <button
         className="w-10 sm:w-16 p-0 m-0 rounded-3xl flex items-center justify-center cursor-pointer transition-transform transform hover:scale-110"
@@ -36,27 +33,13 @@ const LinkHotspotElement: React.FC<LinkHotspotElementProps> = ({
           className="w-10 h-10 sm:w-16 sm:h-16 transition-transform transform hover:scale-110"
         />
       </button>
-      <HotspotContent isHovered={isHovered} title={hotspot.text || ""} />
+      <h2
+        className="lg:hidden group-hover:block drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] min-w-36 lg:min-w-96 -translate-x-1/2 left-1/2 text-center absolute rounded-md shadow-md p-4">
+          {hotspot.text || ""}
+        </h2>
     </div>
   );
 };
 
 export default LinkHotspotElement;
 
-interface HotspotContentProps {
-  isHovered: boolean;
-  title: string;
-}
-
-const HotspotContent: React.FC<HotspotContentProps> = ({
-  title,
-  isHovered,
-}) => {
-  return isHovered ? (
-    <h2
-      className={`drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] min-w-36 lg:min-w-96 -translate-x-1/2 left-1/2 text-center absolute rounded-md shadow-md p-4`}
-    >
-      {title}
-    </h2>
-  ) : null;
-};
